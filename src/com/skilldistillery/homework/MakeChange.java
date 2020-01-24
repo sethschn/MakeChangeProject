7d6277d8297488c1215cc6b106dfebe482e7da50
sethschn <sethschn301@gmail.com>
2020-01-24 16:33:06 -0700

    Initial Git Repo

---
+++ src/com/skilldistillery/homework/MakeChange.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.homework;
+
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Enter the price of the item: $");
+		
+		
+		kb.close();
+
+	}
+
+}
